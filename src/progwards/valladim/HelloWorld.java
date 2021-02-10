981a7c4c20f4ac17accb0fc1fba55bbc0f5f4a49
Vadim <vkodoch@gmail.com>
2021-02-10 22:59:13 +0300

    HelloWorld start

---
+++ src/progwards/valladim/HelloWorld.java
@@ -0,0 +1,12 @@
+package progwards.valladim;
+
+public class HelloWorld {
+	static void helloWorld(){
+		System.out.println("Hello World!");
+	}
+	public static void main(String[] args){
+		helloWorld();
+		HelloWorld.helloWorld();
+		//progwards.valladim.progwards.valladim.HelloWorld.helloWorld();
+	}
+}
