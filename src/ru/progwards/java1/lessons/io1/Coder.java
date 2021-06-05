593042b4692ca68f462959e19b4eeab3ce92abc2
Vadim <vkodoch@gmail.com>
2021-06-05 16:49:33 +0300

    IO step3

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -1,67 +1,51 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Coder {
-	public static void codeFile(String inFileName, String outFileName, char[] code, String logName){
-		int i = 0;
-		int coef;
-		int fileLength = 0;
-		String msg;
-		String result = "";
-		try {
-			FileWriter logFile = new FileWriter(logName, true);
-			try {
-				FileReader fileReader = new FileReader(inFileName);
-				Scanner scanner = new Scanner(fileReader);
-				FileWriter fileWriter = new FileWriter(outFileName, false);
-				while (scanner.hasNextLine()) {
-					String str = scanner.nextLine();
-					fileLength += str.length();
-				}
-				char[] buf = new char[fileLength];
-				if (code.length != 0 && code[0] > 0)
-					coef = code[0];
-				else
-					coef = 15;
-				code = new char[buf.length];
-				try {
-					fileReader = new FileReader(inFileName);
-					for (int ch; (ch = fileReader.read()) >= 0 && i < buf.length; i++) {
-						if (ch == 10 || ch == 13 || ch == 32) {
-							i--;
-						} else {
-							ch += coef;
-							code[i] = (char) ch;
-							String c = Integer.toString(code[i]);
-							result += c + " ";
-						}
-					}
-					fileWriter.write(result);
-				} finally {
-					try {	scanner.close();} catch (Throwable ignored){}
-					try { fileWriter.close();} catch (Throwable ignored){}
-					try {	fileReader.close();} catch (Throwable ignored){}
-				}
-			} catch (Exception e) {
-				msg = e.getMessage();
-				try {
-					logFile.write(msg + "\n");
-				} finally {
-					logFile.close();
-				}
+	public static void toLog(String logName, String msg)	{
+		System.out.println(msg);
+		try	{
+			FileWriter log = new FileWriter(logName, true);
+			try	{
+				log.write(msg + "\n");
+			} finally	{
+				log.close();
 			}
-		} catch (Exception e) {
-			msg = e.getMessage();
-			System.out.println(msg);
+		} catch (Exception e)	{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void codeFile(String inFileName, String outFileName, char[] code, String logName)	{
+		try	{
+			FileInputStream reader = new FileInputStream(inFileName);
+			FileOutputStream writer = new FileOutputStream(outFileName);
+			
+			try	{
+				Arrays.fill(code, '!');
+				for (int i = 48; i < 58; i++)
+					code[i] = (char) (i + 16);
+				byte[] bytes = reader.readAllBytes();
+				for (int i = 0; i < bytes.length; i++)
+					bytes[i] = (byte) code[bytes[i]];
+				writer.write(bytes);
+				
+			} finally	{
+				writer.close();
+				reader.close();
+			}
+		} catch (Exception e)	{
+			toLog(logName, e.getMessage());
 		}
 	}
 	
 	public static void main(String[] args) {
-		char[] code = {};
+		char[] code = new char[256];
 		codeFile("inFile1.txt", "outFile1.txt", code, "logFile.txt");
 	}
 }
