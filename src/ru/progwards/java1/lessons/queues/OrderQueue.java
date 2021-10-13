dcf7447caf84fbee8ec2f849b13767699ce6a516
Vadim <vkodoch@gmail.com>
2021-10-13 18:45:11 +0300

    queues step6

--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -2,10 +2,8 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class OrderQueue {
-	public List<Order> queueOrder;
+	public static LinkedList<Order> orderQueue = new LinkedList<>();
 	public void add(Order order){
-		queueOrder = new ArrayList<>();
-		queueOrder.add(order);
 		if(order.getSum() <= 10000.0)
 			order.priority = 3;
 		else if(order.getSum() > 10000.0 && order.getSum() <= 20000.0)
@@ -20,41 +18,38 @@ public class OrderQueue {
 				return p1.compareTo(p2);
 			}
 		};
-		queueOrder.removeAll(queueOrder);
-		for(Order val : order.listOrder){
-			if(val.priority != 0){
-				queueOrder.add(val);
-			}
-		}
-		Collections.sort(queueOrder, comparator);
-		//System.out.println(queueOrder);
+		Collections.sort(order.listOrder, comparator);
+		Order o = null;
+		orderQueue.add(o);
+		Collections.copy(orderQueue, order.listOrder);
 	}
 	public Order get(){
 		Order order = null;
-		Iterator<Order> iterator = queueOrder.iterator();
-		if(iterator.hasNext())
-			order = iterator.next();
+		if(!orderQueue.isEmpty())
+			return orderQueue.poll();
 		return order;
 	}
 	public static void main(String[] args) {
-		Order o1 = new Order(1500);
-		Order o2 = new Order(21000);
-		Order o3 = new Order(28000);
-		Order o4 = new Order(30000);
-		Order o5 = new Order(12000);
-		Order o6 = new Order(8000);
-		Order o7 = new Order(15000);
-		Order o8 = new Order(10000);
-		OrderQueue orderQueue = new OrderQueue();
-		orderQueue.add(o1);
-		orderQueue.add(o2);
-		orderQueue.add(o3);
-		orderQueue.add(o4);
-		orderQueue.add(o5);
-		orderQueue.add(o6);
-		orderQueue.add(o7);
-		orderQueue.add(o8);
-		System.out.println(orderQueue.queueOrder);
-		System.out.println(orderQueue.get());
+ 		OrderQueue queue = new OrderQueue();
+		queue.add(new Order(1500));
+		queue.add(new Order(20000));
+		queue.add(new Order(28000));
+		queue.add(new Order(13000));
+		queue.add(new Order(22000));
+		queue.add(new Order(8000));
+		queue.add(new Order(12000));
+		queue.add(new Order(10000));
+		queue.add(new Order(30000));
+		System.out.println(queue.orderQueue);
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
+		System.out.println(queue.get());
 	}
 }
