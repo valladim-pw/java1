86d43c3879011b218a737fe1d6d59a433ed1d6eb
Vadim <vkodoch@gmail.com>
2021-10-23 18:15:06 +0300

    queues step7

--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -2,54 +2,64 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class OrderQueue {
-	public static LinkedList<Order> orderQueue = new LinkedList<>();
-	public void add(Order order){
-		if(order.getSum() <= 10000.0)
-			order.priority = 3;
-		else if(order.getSum() > 10000.0 && order.getSum() <= 20000.0)
-			order.priority = 2;
+	public static LinkedList<OrderQueue> orderPriority = new LinkedList<>();
+	public int priority = 0;
+	public double sum;
+	public int num;
+	public OrderQueue addPriority(Order order) {
+		OrderQueue orderQ = new OrderQueue();
+		if (order.getSum() <= 10000.0)
+			orderQ.priority = 3;
+		else if (order.getSum() > 10000.0 && order.getSum() <= 20000.0)
+			orderQ.priority = 2;
 		else
-			order.priority = 1;
-		Comparator<Order> comparator = new Comparator<Order>() {
+			orderQ.priority = 1;
+		orderQ.num = order.getNum();
+		orderQ.sum = order.getSum();
+		return orderQ;
+	}
+	public void add(Order order) {
+		Comparator<OrderQueue> comparator = new Comparator<OrderQueue>() {
 			@Override
-			public int compare(Order o1, Order o2) {
+			public int compare(OrderQueue o1, OrderQueue o2) {
 				Integer p1 = o1.priority;
 				Integer p2 = o2.priority;
 				return p1.compareTo(p2);
 			}
 		};
-		Collections.sort(order.listOrder, comparator);
-		Order o = null;
-		orderQueue.add(o);
-		Collections.copy(orderQueue, order.listOrder);
+		orderPriority.add(this.addPriority(order));
+		Collections.sort(orderPriority, comparator);
+		System.out.println("priority" + orderPriority);
 	}
-	public Order get(){
-		Order order = null;
-		if(!orderQueue.isEmpty())
-			return orderQueue.poll();
+	public OrderQueue get() {
+		OrderQueue order = null;
+		if (!orderPriority.isEmpty())
+			return orderPriority.poll();
 		return order;
 	}
+	@Override
+	public String toString() {
+		return num + ": " + sum + "(" + priority + ")";
+	}
 	public static void main(String[] args) {
- 		OrderQueue queue = new OrderQueue();
-		queue.add(new Order(1500));
-		queue.add(new Order(20000));
-		queue.add(new Order(28000));
-		queue.add(new Order(13000));
-		queue.add(new Order(22000));
-		queue.add(new Order(8000));
-		queue.add(new Order(12000));
-		queue.add(new Order(10000));
-		queue.add(new Order(30000));
-		System.out.println(queue.orderQueue);
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
-		System.out.println(queue.get());
+		OrderQueue orderQueue = new OrderQueue();
+		orderQueue.add(new Order(1500));
+		orderQueue.add(new Order(21000));
+		orderQueue.add(new Order(28000));
+		orderQueue.add(new Order(30000));
+		orderQueue.add(new Order(12000));
+		orderQueue.add(new Order(8000));
+		orderQueue.add(new Order(15000));
+		orderQueue.add(new Order(10000));
+		System.out.println(orderQueue.orderPriority);
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
+		System.out.println(orderQueue.get());
 	}
 }
