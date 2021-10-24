b245e92fba28ef135e9ae147c0467f653e693438
Vadim <vkodoch@gmail.com>
2021-10-24 16:55:41 +0300

    queues step9

--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -2,48 +2,36 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class OrderQueue {
-	public static LinkedList<OrderQueue> orderPriority = new LinkedList<>();
+	public static LinkedList<Order> orderPriority = new LinkedList<>();
 	public int priority = 0;
-	public double sum;
-	public int num;
-	public OrderQueue addPriority(Order order) {
-		Iterator<Order> iterator = order.listOrder.iterator();
-		while(iterator.hasNext()) {
-			order = iterator.next();
-		}
-		OrderQueue orderQ = new OrderQueue();
+	public int addPriority(Order order) {
 		if (order.getSum() <= 10000.0)
-			orderQ.priority = 3;
+			priority = 3;
 		else if (order.getSum() > 10000.0 && order.getSum() <= 20000.0)
-			orderQ.priority = 2;
+			priority = 2;
 		else
-			orderQ.priority = 1;
-		orderQ.num = order.getNum();
-		orderQ.sum = order.getSum();
-		return orderQ;
+			priority = 1;
+		return priority;
 	}
 	public void add(Order order) {
-		Comparator<OrderQueue> comparator = new Comparator<OrderQueue>() {
+		Comparator<Order> comparator = new Comparator<Order>() {
 			@Override
-			public int compare(OrderQueue o1, OrderQueue o2) {
-				Integer p1 = o1.priority;
-				Integer p2 = o2.priority;
+			public int compare(Order o1, Order o2) {
+				Integer p1 = addPriority(o1);
+				Integer p2 = addPriority(o2);
 				return p1.compareTo(p2);
 			}
 		};
-		orderPriority.add(this.addPriority(order));
-		Collections.sort(orderPriority, comparator);
-		//System.out.println("priority" + orderPriority);
+		Collections.sort(order.listOrder, comparator);
+		Order o = null;
+		orderPriority.add(o);
+		Collections.copy(orderPriority, order.listOrder);
 	}
-	public OrderQueue get() {
-		OrderQueue order = null;
+	public Order get() {
+		Order order = null;
 		if (!orderPriority.isEmpty())
 			return orderPriority.poll();
 		return order;
-	}
-	@Override
-	public String toString() {
-		return num + ": " + sum + "(" + priority + ")";
 	}
 	public static void main(String[] args) {
 		OrderQueue orderQueue = new OrderQueue();
