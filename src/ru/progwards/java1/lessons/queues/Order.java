a2c4b25c509b971ff492cd8085993ecc1ade2534
Vadim <vkodoch@gmail.com>
2021-10-26 23:30:17 +0300

    queues step7

--- src/ru/progwards/java1/lessons/queues/Order.java
+++ src/ru/progwards/java1/lessons/queues/Order.java
@@ -4,25 +4,24 @@ import java.util.*;
 public class Order {
 	private double sum;
 	private int num;
-	public static List<Order> listOrder = new ArrayList<>();
-	public Order(double sum){
+	public static int count = 1;
+	
+	public Order(double sum) {
 		this.sum = sum;
-		listOrder.add(this.addOrder());
+		num = count;
+		count++;
 	}
+	
 	public double getSum() {
 		return sum;
 	}
+	
 	public int getNum() {
 		return num;
 	}
-	public Order addOrder(){
-		List<Order> list = new ArrayList<>();
-		list.add(this);
-		this.num = listOrder.size() + 1;
-		return this;
-	}
+	
 	@Override
 	public String toString() {
-		return num + ": " + sum ;
+		return num + ": " + sum;
 	}
 }
