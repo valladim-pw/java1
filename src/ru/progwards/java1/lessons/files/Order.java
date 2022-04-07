86f92525d2ec8d59c22359ac428ee947443b1df6
Vadim <vkodoch@gmail.com>
2022-04-07 12:25:17 +0300

    files step19

--- src/ru/progwards/java1/lessons/files/Order.java
+++ src/ru/progwards/java1/lessons/files/Order.java
@@ -1,74 +1,67 @@
 package ru.progwards.java1.lessons.files;
-import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.FileTime;
 import java.time.*;
 import java.util.*;
 
-public class Order{
-	public static Order loadOrder(Path path, LocalDate start, LocalDate finish, String shopId){
-		try{
-			String filename = path.getFileName().toString();
-			FileTime time = (FileTime) Files.getAttribute(path, "basic:lastModifiedTime");
-			LocalDateTime ldt = LocalDateTime.ofInstant(time.toInstant(), ZoneId.systemDefault());
-			LocalDate localDate = LocalDate.ofInstant(time.toInstant(), ZoneId.systemDefault());
-			if ((start == null || (start!=null && start.compareTo(localDate) <= 0)) &&
-							(finish == null || (finish!=null && finish.compareTo(localDate) >= 0)) &&
-							(shopId == null || (shopId!=null && shopId.compareTo(filename.substring(0, 3)) == 0))){
-				Order order = new Order(filename.substring(0, 3), filename.substring(-2, 10),
-								filename.substring(11, 15), ldt);
-				order.loadItems(path);
-				return order;
-			} else {
-				return null;
-			}
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	private void loadItems(Path path) throws IOException, NumberFormatException {
-		TreeSet<OrderItem> tree = new TreeSet<>(new Comparator<>(){
-			@Override
-			public int compare(OrderItem o1, OrderItem o2)
-			{
-				return o1.googsName.compareTo(o2.googsName);
-			}
-		});
-		sum = 0.0;
-		List<String> lines = Files.readAllLines(path);
-		for (String line : lines){
-			String[] item = line.split(",");
-			String goog = item[0].trim();
-			int count = Integer.parseInt(item[1].trim());
-			double price = Double.parseDouble(item[2].trim());
-			sum = sum + price * count;
-			tree.add(new OrderItem(goog, count, price));
-		}
-		items = new ArrayList(tree);
-	}
-	public Order(){}
-	public Order(String shopId, String orderId, String customerId, LocalDateTime datetime){
-		this.shopId = shopId;
-		this.orderId = orderId;
-		this.customerId = customerId;
-		this.datetime = datetime;
-		items = new ArrayList<>();
-	}
+public class Order {
 	public String shopId;
 	public String orderId;
 	public String customerId;
 	public LocalDateTime datetime;
-	public List<OrderItem> items;
+	public List<OrderItem> items = new ArrayList<>();
 	public double sum;
+	public Order(){}
+	public Order getOrder(Path file){
+		try{
+			this.shopId = file.getFileName().toString().substring(0, 3);
+			this.orderId = file.getFileName().toString().substring(4, 10);
+			this.customerId = file.getFileName().toString().substring(11, 15);
+			this.datetime = LocalDateTime.ofInstant(Files.getLastModifiedTime(file).toInstant(), ZoneId.systemDefault());
+			TreeSet<OrderItem> itemSet = new TreeSet<>(new Comparator<>(){
+				@Override
+				public int compare(OrderItem o1, OrderItem o2){
+					return o1.googsName.compareTo(o2.googsName);
+				}
+			});
+			sum = 0.0;
+			List<String> list = Files.readAllLines(file);
+			try{
+				for(String str : list){
+					String[] strArr = str.split(",");
+					if(strArr.length != 3){
+						this.items = null;
+						return this;
+					}
+					String good = strArr[0].trim();
+					int count = Integer.parseInt(strArr[1].trim());
+					double price = Double.parseDouble(strArr[2].trim());
+					OrderItem orderItem = new OrderItem().getOrderItem(good, count, price);
+					this.sum += count * price;
+					itemSet.add(orderItem);
+					this.items = new ArrayList<>(itemSet);
+				}
+			} catch(NumberFormatException e){
+				this.items = null;
+				return this;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return this;
+	}
+	public LocalDate getDate(){
+		return this.datetime.toLocalDate();
+	}
 	@Override
-	public String toString(){
+	public String toString() {
 		return "Order{" +
 						"shopId='" + shopId + '\'' +
 						", orderId='" + orderId + '\'' +
 						", customerId='" + customerId + '\'' +
-						", datetime=" + datetime +
+						", datetime='" + datetime +'\'' +
 						", sum=" + sum +
-						'}';
+						"}" ;
 	}
 }
 
--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -1,17 +1,25 @@
 package ru.progwards.java1.lessons.files;
 
-public class OrderItem{
-	public OrderItem(){}
-	public OrderItem(String googsName, int count, double price) {
-		this.googsName = googsName;
-		this.count = count;
-		this.price = price;
-	}
+public class OrderItem {
 	public String googsName;
 	public int count;
 	public double price;
+	public OrderItem(){}
+	public OrderItem getOrderItem(String goodsName, int count, double price){
+		this.googsName = goodsName;
+		this.count = count;
+		this.price = price;
+		return this;
+	}
+	public String getGoodsName() {
+		return googsName;
+	}
 	public double getSum()
 	{
 		return count * price;
+	}
+	@Override
+	public String toString() {
+		return googsName + " " + count + " " + price + "\n";
 	}
 }
