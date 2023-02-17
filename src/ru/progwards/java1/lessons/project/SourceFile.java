4a9a859e7912a2c5bc4c6217df5b9ece632e0317
valladim-pw <78878392+valladim-pw@users.noreply.github.com>
2023-02-17 10:33:53 +0300

    Delete SourceFile.java

--- src/ru/progwards/java1/lessons/project/SourceFile.java
+++
@@ -1,9 +0,0 @@
-package ru.progwards.java1.lessons.project;
-
-public class SourceFile extends ProcessFile {
-
-	public SourceFile(String string) {
-		super(string);
-		this.specifyMaxLine();
-	}
-}
